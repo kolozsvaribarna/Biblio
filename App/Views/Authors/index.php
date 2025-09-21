fced1cf530ede95585d25689599728efdc1203c1
Kolozsvári Barnabás <kolozsvari.barnabas2006@tanulo.boronkay.hu>
2025-09-21 19:57:17 +0200

    authors view and navbar styling

--- App/Views/Authors/index.php
+++ App/Views/Authors/index.php
@@ -1,52 +1,44 @@
 <?php
 
-$tableBody = "";
-foreach ($authors as $author)
-{
-    $tableBody .= <<<HTML
-            <tr>
-                <!--<td>{$author->id}</td>-->
-                <td>{$author->first_name} {$author->last_name}</td>
-                <td><p>{$author->bio}</p></td>
+$authorCards = "";
+foreach ($authors as $author) {
+    $authorCards .= <<<HTML
+    <div class="author-card">
+        <!-- Info -->
+        <div class="author-card-info">
+            <h2 class="author-card-name">{$author->first_name} {$author->last_name}</h2>
+            <p class="author-card-bio">{$author->bio}</p>
+        </div>
 
-                <td class='flex float-right'>
-                    <form method='post' action='/authors/edit'>
-                        <input type='hidden' name='id' value='{$author->id}'>
-                        <button type='submit' name='btn-edit' title='Edit'><i class='fa fa-edit'></i></button>
-                    </form>
-                    <form method='post' action='/authors'>
-                        <input type='hidden' name='id' value='{$author->id}'>
-                        <input type='hidden' name='_method' value='DELETE'>
-                        <button type='submit' name='btn-del' title='Delete'><i class='fa fa-trash trash'></i></button>
-                    </form>
-
-                </td>
-            </tr>
-            HTML;
+        <!-- Actions -->
+        <div class="author-card-actions">
+            <form method="post" action="/authors/edit">
+                <input type="hidden" name="id" value="{$author->id}">
+                <button type="submit" name="btn-edit" title="Edit"><i class="fa fa-edit"></i></button>
+            </form>
+            <form method="post" action="/authors">
+                <input type="hidden" name="id" value="{$author->id}">
+                <input type="hidden" name="_method" value="DELETE">
+                <button type="submit" name="btn-del" title="Delete"><i class="fa fa-trash trash"></i></button>
+            </form>
+        </div>
+    </div>
+    HTML;
 }
 
 $html = <<<HTML
-        
-        <h2>Publishers</h2>
-        
-        <table id='' class='' border="1">
-            <thead>
-                <tr>
-                    <!--<th>#</th>-->
-                    <th>Name</th>
-                    <th>Biography</th>
-                    <th>
-                        <form method='post' action='/authors/create'>
-                            <button type="submit" name='btn-plus' title='New'>
-                                <i class='fa fa-plus plus'></i></button>
-                        </form>
-                    </th>
-                </tr>
-            </thead>
-             <tbody>%s</tbody>
-            <tfoot>
-            </tfoot>
-        </table>
-        HTML;
+    <div class="authors-header">
+        <h2>Authors</h2>
+        <form class="table-header" method="post" action="/authors/create">
+            <button type="submit" name="btn-plus" title="New">
+                <i class="fa fa-plus plus"></i>
+            </button>
+        </form>
+    </div>
 
-echo sprintf($html, $tableBody);
+    <div class="authors-grid">
+        %s
+    </div>
+HTML;
+
+echo sprintf($html, $authorCards);
